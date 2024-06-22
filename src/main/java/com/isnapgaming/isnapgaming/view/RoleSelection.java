5cb224d2636217b4d783d43be73c1a6b1ccfd950
Fabio Sessa <fsessa02@gmail.com>
2024-06-22 17:32:50 +0200

    Added redirect servlet for Role Selection

--- src/main/java/com/isnapgaming/isnapgaming/view/RoleSelection.java
+++ src/main/java/com/isnapgaming/isnapgaming/view/RoleSelection.java
@@ -1,4 +1,43 @@
 package com.isnapgaming.isnapgaming.view;
 
-public class RoleSelection {
+import java.io.*;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.isnapgaming.isnapgaming.StorageManagement.DAO.UserDAO;
+import com.isnapgaming.isnapgaming.UserManagement.User;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import javax.sql.DataSource;
+
+@WebServlet(name = "RoleSelection", value = "/RoleSelection")
+public class RoleSelection extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String roleSelected = request.getParameter("role");
+
+        //TO-DELETE
+        System.out.println("Ruolo selezionato: " + roleSelected);
+
+        if (roleSelected == null || roleSelected.isEmpty()) {
+            throw new ServletException("Unknown role");
+        }
+
+        if(roleSelected.equals("Customer") ){
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
+            dispatcher.forward(request, response);
+        }else if(roleSelected.equals("ProductManager")){
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/productManagerDashboard.jsp");
+            dispatcher.forward(request, response);
+        }else if(roleSelected.equals("OrderManager")){
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/orderManagerDashboard.jsp");
+            dispatcher.forward(request, response);
+        }else {
+            throw new ServletException("Unknown role");
+        }
+    }
 }
