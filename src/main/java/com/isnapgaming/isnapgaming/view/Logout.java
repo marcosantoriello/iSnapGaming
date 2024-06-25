ff30ddf6cb709af080473969f007c1495ff3d6af
Fabio Sessa <fsessa02@gmail.com>
2024-06-25 16:40:42 +0200

    Added Logout servlet

---
+++ src/main/java/com/isnapgaming/isnapgaming/view/Logout.java
@@ -0,0 +1,16 @@
+package com.isnapgaming.isnapgaming.view;
+
+import java.io.*;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "Logout", value = "/Logout")
+public class Logout extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        request.getSession().invalidate();
+        response.sendRedirect("index.jsp");
+    }
+}
