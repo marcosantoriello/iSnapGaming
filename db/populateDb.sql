f4a055548e035210aeaae3e5c1deea6ad66f9002
marcosantoriello <santoriellomarco@yahoo.com>
2024-06-29 16:59:31 +0200

    updated populateDb.sql

--- db/populateDb.sql
+++ db/populateDb.sql
@@ -11,3 +11,7 @@ INSERT INTO ProductManager (id) VALUES (2);
 
 INSERT INTO user (username, password, firstName, lastName, dateOfBirth) VALUES ('abianchi@gmail.com', 'hvweu48gkereflsll3', 'Antonio', 'Bianchi', '1990-01-01');
 INSERT INTO Customer (id) VALUES (3);
+
+INSERT INTO product(prodCode, name, softwareHouse, platform, price, quantity, category, pegi, releaseYear, available) values(751, "Fifa23", "EA Games", "PC", 55, 10, "SPORTS", "PEGI3", 2023, true);
+INSERT INTO product(prodCode, name, softwareHouse, platform, price, quantity, category, pegi, releaseYear, available) values(331, "Hell Let Loose", "Team17 Digital Ltd", "PC", 60, 30, "ACTION", "PEGI16", 2019, true);
+INSERT INTO product(prodCode, name, softwareHouse, platform, price, quantity, category, pegi, releaseYear, available) values(652, "Fortnite", "Epic Games", "PS4", 15, 100, "ACTION", "PEGI12", 2017, true);
