2089ac799c768fbe04a9b198d96e77d8ce912ccc
Fabio Sessa <fsessa02@gmail.com>
2024-07-01 13:29:18 +0200

    Added servlet for product details

---
+++ src/main/java/com/isnapgaming/view/ProductDetails.java
@@ -0,0 +1,62 @@
+package com.isnapgaming.view;
+
+import java.io.*;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.isnapgaming.StorageManagement.DAO.UserDAO;
+import com.isnapgaming.UserManagement.User;
+import com.isnapgaming.OrderManagement.Cart;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import javax.sql.DataSource;
+
+import com.isnapgaming.StorageManagement.DAO.ProductDAO;
+import com.isnapgaming.ProductManagement.Product;
+@WebServlet(name = "ProductDetails", value = "/ProductDetails")
+public class ProductDetails extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DataSource ds = (DataSource) request.getServletContext().getAttribute("DataSource");
+        ProductDAO productDAO = null;
+
+        try {
+            productDAO = new ProductDAO(ds);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        Product product = null;
+        int productCode = Integer.parseInt(request.getParameter("prodCode"));
+
+        try {
+            product = productDAO.findByProdCode(productCode);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        request.setAttribute("product", product);
+
+        HttpSession session = request.getSession();
+        String isProductManager = (String) session.getAttribute("role");
+
+        if("ProductManager".equals(isProductManager)){
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/productDetailsPM.jsp");
+            dispatcher.forward(request, response);
+        }else{
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/product.jsp");
+            dispatcher.forward(request, response);
+        }
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
