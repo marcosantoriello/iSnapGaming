711cfad027721a0906a9bf78933f8348253316a8
Fabio Sessa <fsessa02@gmail.com>
2024-07-02 18:15:23 +0200

    Updated servlet with OrderManager methods

--- src/main/java/com/isnapgaming/view/UpdateStatus.java
+++ src/main/java/com/isnapgaming/view/UpdateStatus.java
@@ -2,12 +2,9 @@ package com.isnapgaming.view;
 
 import java.io.*;
 import java.sql.SQLException;
-import java.util.List;
 
 import com.isnapgaming.StorageManagement.DAO.CustomerOrderDAO;
-import com.isnapgaming.StorageManagement.DAO.UserDAO;
-import com.isnapgaming.UserManagement.User;
-import com.isnapgaming.OrderManagement.Cart;
+import com.isnapgaming.UserManagement.OrderManager;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
@@ -26,34 +23,63 @@ public class UpdateStatus extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         DataSource ds = (DataSource) request.getServletContext().getAttribute("DataSource");
+        HttpSession session = request.getSession();
 
         String newStatus = request.getParameter("newStatus");
+        String currentStatus = request.getParameter("currentStatus");
         int orderId = Integer.parseInt(request.getParameter("orderId"));
 
-
-
-        CustomerOrderDAO coDAO = null;
+        CustomerOrderDAO customerOrderDAO = null;
+        CustomerOrder customerOrder = null;
 
         try {
-            coDAO = new CustomerOrderDAO(ds);
+            customerOrderDAO = new CustomerOrderDAO(ds);
+            customerOrder = customerOrderDAO.findByKey(orderId);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
-        try {
-            coDAO.updateStatus(orderId, CustomerOrder.Status.valueOf(newStatus));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        OrderManager orderManager = new OrderManager();
+
+        if("UNDER_PREPARATION".equals(newStatus) && "SHIPPED".equals(currentStatus)){
+            try {
+                orderManager.restoreOrder(customerOrder,ds);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }else if("UNDER_PREPARATION".equals(newStatus) && "READY_FOR_SENDING".equals(currentStatus) ){
+            try {
+                orderManager.replaceProduct(customerOrder,ds);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }else if("UNDER_PREPARATION".equals(newStatus) ){
+            try {
+                orderManager.checkProduct(customerOrder,ds);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }else if("READY_FOR_SENDING".equals(newStatus)){
+            try {
+                orderManager.packProduct(customerOrder,ds);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }else if("SHIPPED".equals(newStatus)){
+            try {
+                orderManager.contactCourier(customerOrder,ds);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }else if("DELIVERED".equals(newStatus)){
+            try {
+                orderManager.confirmDelivery(customerOrder,ds);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
 
-        CustomerOrder order = null;
-        try {
-            order = coDAO.findByKey(orderId);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        request.setAttribute("order", order);
+        request.setAttribute("customerOrder", customerOrder);
 
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/updateStatus.jsp");
         dispatcher.forward(request, response);
