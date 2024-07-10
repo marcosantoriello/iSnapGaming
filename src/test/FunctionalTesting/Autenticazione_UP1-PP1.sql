078cac1ab0172b8c09d8c71b93411e96ca4c94b8
marcosantoriello <santoriellomarco@yahoo.com>
2024-07-10 12:55:25 +0200

    Functional Testing of Add To Cart

---
+++ src/test/FunctionalTesting/Autenticazione_UP1-PP1.sql
@@ -0,0 +1,4 @@
+INSERT INTO user (username, password, firstName, lastName, dateOfBirth) VALUES ('pippo@gmail.com', 'SuperSafePassword1', 'Pippo', 'Ciao', '1990-01-01');
+INSERT INTO Customer (id) VALUES (6);
+INSERT INTO Manager (id) VALUES (6);
+INSERT INTO ProductManager (id) VALUES (6);
